b610a4ce46912f5a9fc22b033e43891b119a5bd7
Jose Alvaro <josealvaro.nw@gmail.com>
2021-11-08 17:45:18 -0600

    DB Clever Cloud

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'clever'),
 
     /*
     |--------------------------------------------------------------------------
@@ -34,6 +34,26 @@ return [
     */
 
     'connections' => [
+
+        'clever' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'bqdykuelldoghtatvx1z-mysql.services.clever-cloud.com'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'bqdykuelldoghtatvx1z'),
+            'username' => env('DB_USERNAME', 'uqkrwkbmoqdxbwyl'),
+            'password' => env('DB_PASSWORD', '5EU2NgvXOaAzQ3ZdQ0x5'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
